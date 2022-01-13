307bc5533271aa797a2bc36b1c6053b67d1743dc
Michal Mrnuštík <michal.mrnustik@outlook.com>
2022-01-13 13:54:48 +0100

    Add configuration to console project

--- ConfigurationSample.Console/Program.cs
+++ ConfigurationSample.Console/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+configurationBuilder
+    .AddJsonFile("appsettings.json")
+    .AddXmlFile("appsettings.xml")
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .AddCommandLine(args);
+
+var configurationRoot = configurationBuilder.Build();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Logging configuration:");
+Console.ForegroundColor = ConsoleColor.Blue;
+var loggingConfigurationSection = configurationRoot.GetRequiredSection("Logging");
+var loggingLogLevelConfigurationSection = loggingConfigurationSection.GetRequiredSection("LogLevel");
+var defaultLogLevelConfigurationValue = loggingLogLevelConfigurationSection["Default"];
+Console.WriteLine($"Logging:LogLevel:Default = {defaultLogLevelConfigurationValue}");
+Console.WriteLine();
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("My key configuration");
+Console.ForegroundColor = ConsoleColor.Blue;
+var myKeyValue = configurationRoot["MyKey"];
+Console.WriteLine($"MyKey = {myKeyValue}");
